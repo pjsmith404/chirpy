0ee544e6160d30ce8a14a6508174a80e1b365e82
pjsmith404 <pjsmith404@gmail.com>
2026-03-23 20:34:19 +1100

    Fix typo in migration

--- sql/schema/003_user_passwords.sql
+++ sql/schema/003_user_passwords.sql
@@ -1,5 +1,5 @@
 -- +goose Up
-ALTER TABLE users ADD COLUMN hashed_passwords text NOT NULL DEFAULT 'unset';
+ALTER TABLE users ADD COLUMN hashed_password text NOT NULL DEFAULT 'unset';
 
 -- +goose Down
-ALTER TABLE users DROP COLUMN hashed_passwords;
+ALTER TABLE users DROP COLUMN hashed_password;
