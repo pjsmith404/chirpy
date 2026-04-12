d24d95766231191a874f496958b28a94dbf68ffe
pjsmith404 <pjsmith404@gmail.com>
2026-04-12 20:10:18 +1000

    Give handler function files more consistent naming

---
+++ handler_create_user.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/google/uuid"
+	"github.com/pjsmith404/chirpy/internal/auth"
+	"github.com/pjsmith404/chirpy/internal/database"
+	"net/http"
+	"time"
+)
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
+}
+
+func (cfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(
+			w,
+			http.StatusInternalServerError,
+			"Couldn't decode parameters",
+			err,
+		)
+		return
+	}
+
+	hash, err := auth.HashPassword(params.Password)
+	if err != nil {
+		respondWithError(
+			w,
+			http.StatusInternalServerError,
+			"Couldn't create user",
+			err,
+		)
+		return
+	}
+
+	user, err := cfg.db.CreateUser(r.Context(), database.CreateUserParams{params.Email, hash})
+	if err != nil {
+		respondWithError(
+			w,
+			http.StatusInternalServerError,
+			"Couldn't create user",
+			err,
+		)
+		return
+	}
+
+	respondWithJson(w, http.StatusCreated, User(user))
+}
+
--- handler_login_user.go
+++ handler_login_user.go
@@ -10,13 +10,6 @@ import (
 	"time"
 )
 
-type User struct {
-	ID        uuid.UUID `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	Email     string    `json:"email"`
-}
-
 type LoggedInUser struct {
 	ID           uuid.UUID `json:"id"`
 	CreatedAt    time.Time `json:"created_at"`
@@ -24,50 +17,6 @@ type LoggedInUser struct {
 	Email        string    `json:"email"`
 	Token        string    `json:"token"`
 	RefreshToken string    `json:"refresh_token"`
-}
-
-func (cfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
-	type parameters struct {
-		Email    string `json:"email"`
-		Password string `json:"password"`
-	}
-
-	decoder := json.NewDecoder(r.Body)
-	params := parameters{}
-	err := decoder.Decode(&params)
-	if err != nil {
-		respondWithError(
-			w,
-			http.StatusInternalServerError,
-			"Couldn't decode parameters",
-			err,
-		)
-		return
-	}
-
-	hash, err := auth.HashPassword(params.Password)
-	if err != nil {
-		respondWithError(
-			w,
-			http.StatusInternalServerError,
-			"Couldn't create user",
-			err,
-		)
-		return
-	}
-
-	user, err := cfg.db.CreateUser(r.Context(), database.CreateUserParams{params.Email, hash})
-	if err != nil {
-		respondWithError(
-			w,
-			http.StatusInternalServerError,
-			"Couldn't create user",
-			err,
-		)
-		return
-	}
-
-	respondWithJson(w, http.StatusCreated, User(user))
 }
 
 func (cfg *apiConfig) handlerLoginUser(w http.ResponseWriter, r *http.Request) {
